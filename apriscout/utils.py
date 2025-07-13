240fec3fe739ea4183560e926edc33a5112bfc03
Adam Hennessy <ahenneuk@live.com>
2025-07-13 03:25:16 +0100

    Move nav

--- apriscout/utils.py
+++ apriscout/utils.py
@@ -2,6 +2,7 @@
 
 import re
 
+
 def is_valid_username(username: str, max_length=32) -> bool:
     """Username alphanumberic and underscores only."""
     return re.fullmatch(rf"[A-Za-z0-9_]{{1,{max_length}}}", username) is not None
