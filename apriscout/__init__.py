2d308d12c7deb3dbda7d03ae21d838a5707bd9f5
Adam Hennessy <ahenneuk@live.com>
2025-07-13 14:11:27 +0100

    add pokemon table stuff

--- apriscout/__init__.py
+++ apriscout/__init__.py
@@ -8,6 +8,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -30,8 +31,9 @@ def create_app(test_config=None):
     login_manager.init_app(app)
     login_manager.login_view = "login"
 
+    migrate = Migrate(app, db)
+
     from .routes import main
 
     app.register_blueprint(main)
-
     return app
--- apriscout/models.py
+++ apriscout/models.py
@@ -21,6 +21,10 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
 
+    collections = db.relationship(
+        "UserPokemon", back_populates="user", cascade="all, delete-orphan",
+    )
+
     def set_password(self, password):
         """Hash and set a password."""
         self.password_hash = generate_password_hash(password)
@@ -34,3 +38,51 @@ class User(db.Model, UserMixin):
 def load_user(user_id):
     """Return the user instance."""
     return User.query.get(int(user_id))
+
+
+class Pokemon(db.Model):
+    """Master table of Pokemon and details.
+
+    Attributes:
+        id (int): Internal unique ID for each Pokemon.
+        name (str): Pokemon name.
+        dexNum (int): Pokedex number.
+        formId (str): Special forms, like female or gimicks.
+        smogon (str): Smogon name.
+        isFemale (bool): If the Pokemon is (specifically) female.
+        hiddenAbility (bool): If the Pokemon has a hidden ability.
+        abilities (str): The Pokemon's hidden ability, or otherwise first ability.
+        sprite (str): The link to the Pokemon's sprite.
+    """
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True, nullable=False)
+    dex_num = db.Column(db.Integer)
+    form_id = db.Column(db.String(10))
+    is_female = db.Column(db.Boolean)
+    generation = db.Column(db.Integer)
+    type1 = db.Column(db.String(10))
+    type2 = db.Column(db.String(10))
+    sprite = db.Column(db.String(255))
+
+
+class UserPokemon(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    pokemon_id = db.Column(db.Integer, db.ForeignKey("pokemon.id"), nullable=False)
+
+    ball_fast = db.Column(db.Boolean, default=False)
+    ball_lure = db.Column(db.Boolean, default=False)
+    ball_level = db.Column(db.Boolean, default=False)
+    ball_heavy = db.Column(db.Boolean, default=False)
+    ball_love = db.Column(db.Boolean, default=False)
+    ball_moon = db.Column(db.Boolean, default=False)
+    ball_dream = db.Column(db.Boolean, default=False)
+    ball_beast = db.Column(db.Boolean, default=False)
+    ball_safari = db.Column(db.Boolean, default=False)
+    ball_sport = db.Column(db.Boolean, default=False)
+
+    user = db.relationship("User", back_populates="collections")
+    pokemon = db.relationship("Pokemon")
