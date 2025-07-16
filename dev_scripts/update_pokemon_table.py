b07653c45801f954b6ac61490bdc5ea47ac85371
Adam Hennessy <ahenneuk@live.com>
2025-07-16 16:17:33 +0100

    remove pokemon w/ problem sprites

--- dev_scripts/update_pokemon_table.py
+++ dev_scripts/update_pokemon_table.py
@@ -4,11 +4,32 @@ import os
 import json
 from apriscout import create_app, db
 from apriscout.models import Pokemon
+import glob
 
 SPRITE_FOLDER = "sprites/pokemon"
 JSON_PATH = "dev_scripts/legacy-pokemon.json"
 
 app = create_app()
+
+
+def get_missing_sprites():
+    """Find missing Pokemon sprites by cycling through images."""
+
+    filepath = "/Users/ah724/Documents/apriscout/apriscout/static/sprites/pokemon/"
+
+    no_sprite = []
+    for i in range(1, 1533):
+        if not os.path.isfile(filepath + str(i) + ".png"):
+            no_sprite.append(i)
+
+    return no_sprite
+
+
+def get_problem_sprites():
+    """Just a list of problematic Pokemon/sprites."""
+
+    return [x for x in range(1528, 1551)] + [1600]
+
 
 with app.app_context():
 
@@ -18,9 +39,16 @@ with app.app_context():
     with open(JSON_PATH, "r") as f:
         data = json.load(f)
 
+    missing_sprites = get_missing_sprites()
+    problem_sprites = get_problem_sprites()
+
     for id, entry in enumerate(data):
 
         id += 1
+
+        if id in missing_sprites or id in problem_sprites:
+            continue
+
         name = entry["name"]
         dex_num = entry["dexNum"]
         form_id = entry["formId"]
